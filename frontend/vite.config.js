d229587e226d8e8953d31c88d8fe320a4c5c3106
THIAGO DA SILVA <thiagosilva785@gmail.com>
2026-08-19 00:28:02 -0300

    feat: update package-lock files to remove unnecessary dev flags and improve CSS height properties to fix mobile scroll bug

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   publicDir: 'public',
   server: {
-    port: 5173
+    port: 5173,
+    // allowedHosts: true
   },
   build: {
     outDir: 'dist'
